3cf67f737d17153820cd87567cfac3c52f6a8adf
yuichiro <yuichiro@yu16mba.local>
2019-11-27 14:42:49 +0900

    test

---
+++ car/orient.py
@@ -0,0 +1,21 @@
+import airsim
+import cv2
+import numpy as np
+import os
+import setup_path
+import time
+
+# connect to the AirSim simulator
+client = airsim.CarClient()
+client.confirmConnection()
+client.enableApiControl(True)
+car_controls = airsim.CarControls()
+
+q = airsim.Quaternionr()
+print(client.simGetCameraInfo("0"))
+print(client.simGetCameraInfo("4"))
+
+#restore to original state
+client.reset()
+
+client.enableApiControl(False)
