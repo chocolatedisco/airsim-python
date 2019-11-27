871086b95b5cf349bcb4f6f0852647bfdbf7b98d
yuichiro <yuichiro@yu16mba.local>
2019-11-27 15:42:29 +0900

    test

---
+++ car/get_image.py
@@ -0,0 +1,28 @@
+import airsim
+import cv2
+import numpy as np
+import os
+import setup_path 
+import time
+
+# connect to the AirSim simulator
+client = airsim.CarClient()
+client.confirmConnection()
+client.enableApiControl(True)
+
+if not os.path.exists('c:/temp/'):
+    os.makedirs('c:/temp/')
+
+for i in range(0,5):
+    print(i)
+    filename = 'c:/temp/py' + str(i)
+    png_image = client.simGetImage(str(i), airsim.ImageType.Scene)
+    airsim.write_file(os.path.normpath(filename+".png"),png_image)
+
+#restore to original state
+client.reset()
+
+client.enableApiControl(False)
+
+
+            
